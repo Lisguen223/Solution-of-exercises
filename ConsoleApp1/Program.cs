72672daa98d3f82bb3eb1a4c10cd754bb41f931d
lisguen223 <166881828+Lisguen223@users.noreply.github.com>
2025-02-14 09:21:58 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 10; 
+            float b = 38;
+            float c = (31 - 5 * a) / b;
+            Console.WriteLine(c);
+        }
+    }
+}
